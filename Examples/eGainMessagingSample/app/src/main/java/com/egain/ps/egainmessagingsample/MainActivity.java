def8e7fc4d1749a05810fd5d36daf8c124408cc0
Dylan Carr <dcarr@egain.com>
2022-04-25 11:24:52 -0700

    fixed error in customized approach initalize method

--- Examples/eGainMessagingSample/app/src/main/java/com/egain/ps/egainmessagingsample/MainActivity.java
+++ Examples/eGainMessagingSample/app/src/main/java/com/egain/ps/egainmessagingsample/MainActivity.java
@@ -3,6 +3,9 @@ package com.egain.ps.egainmessagingsample;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.egain.ps.sdk.internal.data.models.eGainMessaging;
+import com.egain.ps.sdk.internal.data.ui.brandingUtil;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -14,13 +17,13 @@ public class MainActivity extends AppCompatActivity {
 
         eGainMessaging launchCustomer_qa = new eGainMessaging(
                 this,
-                "",
-                "",
-                "",
-                "",
-                "",
-                "",
-                ""
+                "1fbaa4959ceb4eb59018f5d686f9e8de",
+                "6bOSTWALgUSfEMDRGKmXO8@WAWwbV0pW-XJCYn3t-05OF75mOwiBCKYpsEwR",
+                "mobile_sdk_qa",
+                "mobile_sdk_address_with_egain_bot_qa",
+                "beta-qa@email.com",
+                "beta-qa",
+                "Hi"
         );
     }
 }
