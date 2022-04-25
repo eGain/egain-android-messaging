548623b261a1b71d8599b62ddb5778e8cf098264
Dylan Carr <dcarr@egain.com>
2022-04-25 11:44:59 -0700

    removed client credentials

--- Examples/eGainMessagingSample/app/src/main/java/com/egain/ps/egainmessagingsample/MainActivity.java
+++ Examples/eGainMessagingSample/app/src/main/java/com/egain/ps/egainmessagingsample/MainActivity.java
@@ -17,13 +17,13 @@ public class MainActivity extends AppCompatActivity {
 
         eGainMessaging launchCustomer_qa = new eGainMessaging(
                 this,
-                "1fbaa4959ceb4eb59018f5d686f9e8de",
-                "6bOSTWALgUSfEMDRGKmXO8@WAWwbV0pW-XJCYn3t-05OF75mOwiBCKYpsEwR",
-                "mobile_sdk_qa",
-                "mobile_sdk_address_with_egain_bot_qa",
-                "beta-qa@email.com",
-                "beta-qa",
-                "Hi"
+                "",
+                "",
+                "",
+                "",
+                "",
+                "",
+                ""
         );
     }
 }
